c8bb5c0feed053212f0b07a4fd97574dbc860343
zakarie <zakarie.abdi@student.moringaschool.com>
2025-10-28 13:08:50 +0300

    Update API config to support both local and deployed backend

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,7 +1,10 @@
 // src/Config.jsx
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://127.0.0.1:5000" // local backend
+    : "https://agrolink-backend-kjb1.onrender.com"; // deployed backend
 
 const api = axios.create({
   baseURL: BASE_URL,
